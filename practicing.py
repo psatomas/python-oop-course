4aab80c45ab4f25570bfd0c544f4789297c91cc4
psatomas <psatomas@gmail.com>
2025-02-15 17:15:26 -0300

    feat: updated practincing.py

--- practicing.py
+++ practicing.py
@@ -1,7 +1,10 @@
 class User:
-    def __init__(self, username, password):
-        username = username
-        password = password
+    def __init__(self, username, age):
+        self.username = username
+        self.age = age
+    def presentation(self):
+        print(f"your name is {self.username} and you are {self.age}")
 
-    print(f"{username}")
-        
+
+user1 = User("tomás", 32)
+user1.presentation()
